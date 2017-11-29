18c5a0e2a18eefa9393c4552222905872ecd6c4d
TalAizikov <31697771+TalAizikov@users.noreply.github.com>
2017-11-29 11:56:40 -0500

    Create 6LED.java

---
+++ 6LED.java
@@ -0,0 +1,52 @@
+/* SimpleToggle_PB_LED */
+
+
+const byte LEDpin = 3;
+const byte SWITCHpin = 2;
+const byte LEDpin2 = 4;
+
+byte LEDstate = HIGH;
+byte LEDstate2 = LOW;
+
+byte SWITCHstate;
+byte lastSWITCHstate;
+
+long previousMillis= 0;
+long currentMillis= 0;
+long interval= 1000;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(LEDpin, OUTPUT);
+pinMode(SWITCHpin, INPUT);
+digitalWrite(SWITCHpin, HIGH);
+  //second led
+pinMode(LEDpin2, OUTPUT);
+pinMode(SWITCHpin, INPUT);
+digitalWrite(SWITCHpin, HIGH);
+
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+SWITCHstate = digitalRead(SWITCHpin);
+Serial.print(currentMillis);
+currentMillis=millis();
+if (currentMillis-previousMillis>interval)
+{
+
+if ((lastSWITCHstate == HIGH) && (SWITCHstate == LOW))
+{
+    LEDstate = !LEDstate;
+    LEDstate2 = !LEDstate2;
+    
+    digitalWrite(LEDpin, LEDstate);
+    digitalWrite(LEDpin2, LEDstate2);
+    previousMillis= currentMillis;
+}
+}
+
+lastSWITCHstate = SWITCHstate;
+}
